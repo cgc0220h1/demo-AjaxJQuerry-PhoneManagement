d32da7a495f11c00eeefef7baca357a39285f5ae
Duc <vanduc2514@gmail.com>
2020-06-12 11:51:51 +0700

    Thêm API findOne Thêm Test API

--- src/main/java/api/SmartPhoneAPI.java
+++ src/main/java/api/SmartPhoneAPI.java
@@ -8,9 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.phone.SmartPhoneService;
 import service.phone.exception.NotFoundException;
 
@@ -29,6 +27,17 @@ public class SmartPhoneAPI {
     @GetMapping()
     public ResponseEntity<List<SmartPhone>> getAllPhones() throws NotFoundException {
         List<SmartPhone> list = smartPhoneService.findAll();
-        return new ResponseEntity<>(list, HttpStatus.OK);
+        return new ResponseEntity<>(list, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SmartPhone> getOne(@PathVariable("id") Long id) throws NotFoundException {
+        SmartPhone smartPhone = smartPhoneService.findOne(id);
+        return new ResponseEntity<>(smartPhone, HttpStatus.FOUND);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleNotFoundException() {
     }
 }
