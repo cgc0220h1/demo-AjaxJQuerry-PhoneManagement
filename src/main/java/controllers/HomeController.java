f5321aa941e28e9ee320160b35416989d2899680
Duc <vanduc2514@gmail.com>
2020-06-13 16:51:21 +0700

    1. Thay đổi giao diện trang index, thêm chức năng xoá 2. Viết jQuery cho chức năng xoá

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,10 +1,5 @@
 package controllers;
 
-import model.SmartPhone;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
